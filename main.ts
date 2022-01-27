bc88127c188e00a86d4a8dfc9094a4145308aa1b
Myles-Trump <82889818+Myles-Trump@users.noreply.github.com>
2022-01-27 12:56:16 -0500

    Move

--- main.ts
+++ main.ts
@@ -1,17 +1,8 @@
-input.onPinPressed(TouchPin.P0, function () {
-    basic.showIcon(IconNames.Yes)
-})
 input.onButtonPressed(Button.A, function () {
     robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
+    robotbit.StpCarMove(10, 48)
 })
 input.onButtonPressed(Button.B, function () {
     robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B0)
