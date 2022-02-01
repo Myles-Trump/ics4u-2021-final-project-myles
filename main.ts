eae871547550141708c4492779ee2cd28a07b963
Myles-Trump <82889818+Myles-Trump@users.noreply.github.com>
2022-02-01 13:22:24 -0500

    Vehicle now moves

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
-input.onButtonPressed(Button.A, function () {
-    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
-})
-input.onButtonPressed(Button.AB, function () {
-    robotbit.StpCarMove(10, 48)
-})
-input.onButtonPressed(Button.B, function () {
-    robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B0)
-})
-robotbit.StpCarMove(10, 48)
+robotbit.MotorRunDual(
+robotbit.Motors.M1A,
+200,
+robotbit.Motors.M2A,
+255
+)
+robotbit.StpCarMove(475, 60)
+robotbit.StpCarTurn(-90, 60, 90)
+robotbit.StpCarMove(300, 60)
+music.playMelody("F G F G F G F G ", 500)
+music.playMelody("F G F G F G F G ", 500)
+music.playMelody("F G F G F G F G ", 500)
 basic.forever(function () {
-	
+    robotbit.StpCarTurn(360, 60, 90)
+    robotbit.StpCarTurn(-360, 60, 90)
 })
